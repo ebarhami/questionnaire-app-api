f8d4641dd2d07c41a9c756c03304f8f0103abf13
ebarhami <ensof.intern@stoqo.com>
2020-11-11 17:07:50 +0700

    delete password

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
-const MONGODB_URI = `mongodb+srv://questionnaire:3BwAvUjKHml262WP@cluster0.o8gd8.mongodb.net/questionnaire?retryWrites=true&w=majority`;
+const MONGODB_URI = `mongodb+srv://questionnaire:xxxxx@cluster0.o8gd8.mongodb.net/questionnaire?retryWrites=true&w=majority`;
 
 module.exports = MONGODB_URI;
